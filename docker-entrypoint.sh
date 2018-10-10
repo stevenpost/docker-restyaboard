97a64f3852d630d16d5a0773e726d3e3395f0a69
Steven Post <steven.post@culsans.be>
2018-10-11 00:59:37 +0200

    Add missing option for postfix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -41,6 +41,7 @@ if [ "$1" = 'start' ]; then
       -e '$ a smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt' \
       -e '$ a smtp_tls_wrappermode = yes' \
       -e '$ a smtp_tls_security_level = encrypt' \
+      -e '$ a smtp_tls_note_starttls_offer = yes' \
       /etc/postfix/main.cf
 
   # init db
